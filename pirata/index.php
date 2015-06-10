d08d74c43ea15d36e0fed6538894a96a4bf659b0
Iuri Guilherme <iuri@riseup.net>
2015-06-10 14:37:48 -0300

    Adicionei loop

--- pirata/index.php
+++ pirata/index.php
@@ -2,5 +2,15 @@
 ## Cabeçalho
 <?php get_header(); ?>
 
+## Loop
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<?php endwhile; else : ?>
+	<p><?php _e( 'Nenhuma publicação ainda.', 'piratas' ); ?></p>
+<?php endif; ?>
+
+## Barra lateral
+<?php get_sidebar(); ?>
+
 ## Rodapé
 <?php get_footer(); ?>
